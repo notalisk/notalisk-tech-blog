a1f929fc8ad47429093447b5f0cc62bd9ca29b06
notalisk <81662512+notalisk@users.noreply.github.com>
2023-05-26 11:11:39 -0500

    finish relationships in index.js

--- models/index.js
+++ models/index.js
@@ -4,11 +4,35 @@ const Comment = require('./Comment');
 
 // Users have many Blogs
 // Blogs have one User
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 // Users have many Comments
 // Comments have one User
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user'
+});
 
 // Blogs have many Comments
 // Comments have one Blog
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
 
 module.exports = { User, Blog, Comment };
